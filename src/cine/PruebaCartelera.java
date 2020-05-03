821b3774f0319da1cf097400148829e889c82718
Mariano Tugnarelli <mariano.tugnarelli@grupoesfera.com.ar>
2020-05-03 16:52:19 -0300

    Obtiene el primer elemento de la lista

--- src/cine/PruebaCartelera.java
+++ src/cine/PruebaCartelera.java
@@ -14,5 +14,11 @@ public class PruebaCartelera {
 		List<Pelicula> peliculas = cartelera.obtenerPeliculas();
 		
 		Assert.assertFalse(peliculas.isEmpty());
+		
+		Pelicula primerPelicula = peliculas.get(0);
+		
+		Assert.assertEquals("Matrix", primerPelicula.obtenerTitulo());
+		Assert.assertEquals(Genero.ACCION, primerPelicula.obtenerGenero());
+		Assert.assertEquals(136, primerPelicula.obtenerDuracion());
 	}
 }
