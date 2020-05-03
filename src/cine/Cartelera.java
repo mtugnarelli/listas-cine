efb0a1bf387af674e4b2a6ee00f3a1e5b7f95038
Mariano Tugnarelli <mariano.tugnarelli@grupoesfera.com.ar>
2020-05-03 16:53:08 -0300

    Completa la información de la primer película

--- src/cine/Cartelera.java
+++ src/cine/Cartelera.java
@@ -14,12 +14,29 @@ public class Cartelera {
 	public List<Pelicula> obtenerPeliculas() throws ExcepcionEnElCine {
 		
 		List<Pelicula> peliculas = new LinkedList<Pelicula>();
-
-		Pelicula pelicula = new Pelicula(null, null, 0);
+		
+		Pelicula pelicula = crearPelicula("  Matrix  , ACCION  , 136  ");
 		
 		peliculas.add(pelicula);
 		
 		return peliculas;
+	}
+	
+	private Pelicula crearPelicula(String linea) throws ExcepcionEnElCine {
 
+		String[] campos = linea.split(",");
+		
+		try {
+			
+			String titulo = campos[0].trim();
+			Genero genero = Genero.valueOf(campos[1].trim());
+			int duracion = Integer.parseInt(campos[2].trim());
+			
+			return new Pelicula(titulo, genero, duracion);
+			
+		} catch (Exception e) {
+			
+			throw new ExcepcionEnElCine("El formato es incorrecto", e);
+		}
 	}
 }
